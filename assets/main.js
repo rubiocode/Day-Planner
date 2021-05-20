4342848322752285927b9e89000066ff45d9d05f
rubiocode <78938193+rubiocode@users.noreply.github.com>
2021-05-19 23:18:40 -0700

    local storage

--- assets/main.js
+++ assets/main.js
@@ -37,60 +37,91 @@ let scheduleTime = {
         };
     })
 
-    // setting up local storage and render todo list
-  
-    //create an object - make a key for each hour, set their value to empty string, will be saved in the local storage
-  
-  //change the scheduleObj to the item saved in the local storage
+    /* setting up local storage for each button (prev class method did not work doing everything by ID)*/
 
-    /*function init() {
-    let storedTodos = JSON.parse(localStorage.getItem('todos'));
-    if (storedTodos !== null) {
-        scheduleTime = storedTodos;
-    }
-    //change the text inside the textarea to the local storage item
-    let todosValue = Array.from($('inpValue'));
-    todosValue.forEach(element => {
-        let timeValue = element.getAttribute('text-value');
-        element.textContent(scheduleTime[timeValue]);
-    });
-}
-init();*/
+const btn1El = document.getElementById("btn1");
+const btn2El = document.getElementById("btn2");
+const btn3El = document.getElementById("btn3");
+const btn4El = document.getElementById("btn4");
+const btn5El = document.getElementById("btn5");
+const btn6El = document.getElementById("btn6");
+const btn7El = document.getElementById("btn7");
+const btn8El = document.getElementById("btn8");
+const btn9El = document.getElementById("btn9");
 
 
-const saveBtn = document.querySelectorAll("myClass");
-const todoList = document.querySelector(".container");
-const todoInput = document.querySelectorAll(".inpValue");
+
+
+
+btn1El.addEventListener('click', myFunction)
+btn2El.addEventListener('click', myFunction)
+btn3El.addEventListener('click', myFunction)
+btn4El.addEventListener('click', myFunction)
+btn5El.addEventListener('click', myFunction)
+btn6El.addEventListener('click', myFunction)
+btn7El.addEventListener('click', myFunction)
+btn8El.addEventListener('click', myFunction)
+btn9El.addEventListener('click', myFunction)
+
+
+
+const text1El = document.getElementById("text1");
+const text2El = document.getElementById("text2");
+const text3El = document.getElementById("text3");
+const text4El = document.getElementById("text4");
+const text5El = document.getElementById("text5");
+const text6El = document.getElementById("text6");
+const text7El = document.getElementById("text7");
+const text8El = document.getElementById("text8");
+const text9El = document.getElementById("text9");
+
+
+
+text1El.addEventListener('keyup', ()=>{
+    btn1El.disabled = !text1El.value;   
+});
+text2El.addEventListener('keyup', ()=>{
+    btn2El.disabled = !text2El.value;   
+});
+text3El.addEventListener('keyup', ()=>{
+    btn3El.disabled = !text3El.value;   
+});
+text4El.addEventListener('keyup', ()=>{
+    btn4El.disabled = !text4El.value;   
+});
+text5El.addEventListener('keyup', ()=>{
+    btn5El.disabled = !text5El.value;   
+});
+text6El.addEventListener('keyup', ()=>{
+    btn6El.disabled = !text6El.value;   
+});
+text7El.addEventListener('keyup', ()=>{
+    btn7El.disabled = !text7El.value;   
+});
+text8El.addEventListener('keyup', ()=>{
+    btn8El.disabled = !text8El.value;   
+});
+text9El.addEventListener('keyup', ()=>{
+    btn9El.disabled = !text9El.value;   
+});
+
 let todos=[];
 
-
-function init(){
-    let saveTodos= JSON.parse(local.getItem("todos"));
-    if (saveTodos !==null) {
-        todos = saveTodos;
-    }
-}
-
-init();
-
-
-function saveTodos () {
-
-    localStorage.setItem('todos', JSON.stringify(todos));
-
-}
-
-saveBtn.addEventListener ('click', function(event){
+function myFunction (event) {
     event.preventDefault();
+    let todoInput= $(".inpValue").val();
+    localStorage.setItem("todos", JSON.stringify(todoInput));
+    renderTodos();
+}
+
+function renderTodos() {
     
-        todoInput= todos.values.trim();
-
-    if (todoInput ==="") {
-        return;
-    }
-    todos.push(todoInput);
-    todos.value="";
+    inpValue.innerHTML = todoInput;
+    todoInput.push(todos);
+    
+}
 
 
-    saveTodos();
-});
+
+let inputValue = document.getElementsByClassName("inpValue");
+let todoInput= JSON.parse(localStorage.getItem('todos'));
